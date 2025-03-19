4d7aca964110a9fdce451c188e3ee8e2bcdad564
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-03-19 14:32:49 -0500

    Update HistoryScreen.kt

--- AppMiCalculadora/HistoryScreen.kt
+++ AppMiCalculadora/HistoryScreen.kt
@@ -1,15 +1,31 @@
-package com.example.appcalculadora
+package com.example.appcalculadora.main
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+import androidx.compose.runtime.livedata.observeAsState
+
 @Composable
-fun HistoryScreen(navController: NavController) {
+fun HistoryScreen(navController: NavController, viewModel: CalculadoraViewModel) {
+
+    val history by viewModel.history.observeAsState(emptyList())
+
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+
+        Text(text="Historial de cálculos", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        history.forEach{
+            calculation ->
+            Text(text=calculation, style = MaterialTheme.typography.bodyLarge)
+            Divider()
+        }
+
         Text(text = "Historial de cálculos", fontSize = 24.sp)
         Button(onClick = { navController.popBackStack() }) {
             Text("Volver")
