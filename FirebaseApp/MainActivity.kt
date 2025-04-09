4379cf12e701263df8a5ae07a1ab12f6dd3c4579
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-04-09 14:42:04 -0500

    Update MainActivity.kt

--- FirebaseApp/MainActivity.kt
+++ FirebaseApp/MainActivity.kt
@@ -1,7 +1,27 @@
+package com.example.firebaseapp
+
 import android.os.Bundle
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firebaseapp.ui.theme.FirebaseAppTheme
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
 
